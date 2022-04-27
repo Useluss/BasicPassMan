54d2099ef8b0237ea02dcbc0701e50cf3df2816c
Ryan Fiset <ryanfiset256@gmail.com>
2022-04-26 23:00:56 -0700

    Create user and Account classes

---
+++ BasicPassMan/src/UserUtils/Account.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BasicPassMan.UserUtils
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
---
+++ BasicPassMan/src/UserUtils/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace BasicPassMan.UserUtils
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] Salt { get; set; }
+        public List<Account> Accounts { get; set; }
+    }
+}
