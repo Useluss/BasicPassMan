1bd321c398b29badab941532c0bc17abd066c51e
Ryan Fiset <ryanfiset256@gmail.com>
2022-04-26 23:42:14 -0700

    Added a test for new code

--- BasicPassMan/src/Program.cs
+++ BasicPassMan/src/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using BasicPassMan.JSON;
+using System.Text;
 using BasicPassMan.JSON.Builder;
+using BasicPassMan.JSON.Encryptor;
 using BasicPassMan.JSON.Writer;
+using BasicPassMan.UserUtils;
 
 namespace BasicPassMan
 {
@@ -9,6 +11,19 @@ namespace BasicPassMan
     {
         static void Main(string[] args)
         {
+            var encryptor = new JsonFileEncrypt();
+
+            var user = new User
+            {
+                Username = "Useluss", // Temp declaration 
+                UserEmail = "ryanfiset256@gmail.com", // Temp declaration
+                Accounts = null
+            };
+            Console.WriteLine("Please enter a password: ");
+            var password = Console.ReadLine() + user.Username + user.UserEmail;
+            
+            encryptor.Encrypt(null, ref user, Encoding.ASCII.GetBytes(password));
+
             var userJson = JsonBuilder.CreateJsonUserObject();
             JsonWriter.WriteJson(userJson);
         }
